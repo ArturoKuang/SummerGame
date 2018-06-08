decf0a675bd2d92fa624fb0b8a6a0522881f9c19
Keith Woo <keithwoo71@gmail.com>
2018-06-07 21:38:53 -0400

    Added new animations for enemy, and they all work. Haven't tested death.

--- SummerProject/Assets/Scripts/Enemy/MeleeEnemy.cs
+++ SummerProject/Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -7,18 +7,20 @@ public class MeleeEnemy : MonoBehaviour, IEnemy {
 
     [SerializeField]
     public Transform target;
-    protected NavMeshAgent ThisAgent = null;
-    private float meleeCD = 1.0f;
-    private float meleeDistance = 1.5f;
-    private float meleeDamage = 10;
-
+    public float meleeDistance = 1.5f;
+    public float aggroDistance = 8f;
+    public float meleeCD = 1.0f;
+    public float meleeDamage = 10;
     private float meleeStartTime = float.MinValue;
 
+    protected NavMeshAgent ThisAgent = null;
+    
     public float health;
     public Rigidbody rb;
 
-    public GameObject player;
-    public Player playerScript;
+    private  GameObject player;
+    private Player playerScript;
+    private Animator animator;
 
 
     /**
@@ -32,6 +34,7 @@ public class MeleeEnemy : MonoBehaviour, IEnemy {
         ThisAgent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
         playerScript = player.GetComponent<Player>();
+        animator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -40,18 +43,27 @@ public class MeleeEnemy : MonoBehaviour, IEnemy {
             Death();
         }
 
-        if (InRange()) {
-            if (canAttack()) {
-                Debug.Log("COLLIDED");
-                // Perform attack animation here and call Attack();
-                Attack();
+        // Check if enemy will attack or move
+        float curr_distance = CheckPlayerDistance();
+        if (curr_distance <= aggroDistance) {
+            if (curr_distance <= meleeDistance) {
+                if (canAttack()) {
+                    Debug.Log("PLAYER HIT");
+                    Attack();   // Perform attack animation here and call Attack();
+                }
+            } else {
+                animator.SetBool("Idle", false);
+                ResetAnimations();
+                Move();
             }
         }
         else {
-            Move();
+            if (animator.GetBool("Idle") == false) {
+                ResetAnimations();
+                animator.SetBool("Idle", true);
+            }
         }
 
-        
 	}
 
     /**
@@ -64,10 +76,11 @@ public class MeleeEnemy : MonoBehaviour, IEnemy {
     }
 
     /**
-     * Simply move towards the player
+     * Simply move towards the player using navmesh
      */
     public void Move() {
         ThisAgent.SetDestination(target.position);
+        SetWalkingAnimation();
     }
 
     /**
@@ -75,25 +88,60 @@ public class MeleeEnemy : MonoBehaviour, IEnemy {
      */
     public void Death() {
         Animation death = GetComponent<Animation>(); // Need to get actual animations lol
-        death.Play();
+        animator.SetTrigger("Death");
         Destroy(gameObject, death.clip.length);
     }
 
     /**
-     * Checks if enemy is in range of player to perform an attack
+     * Checks to see if the melee cool down has refreshed or not
      */
-    private bool InRange() {
-        if (Vector3.Distance(target.position, transform.position) <= meleeDistance) {
-            return true;
-        }
-
-        return false;
-    }
-
     private bool canAttack() {
         // Add animation duration later
         return (Time.time - meleeStartTime) > meleeCD;
     }
+
+    /**
+     * Returns Distance between enemy and player
+     */
+    private float CheckPlayerDistance() {
+        return Vector3.Distance(target.position, transform.position);
+    }
+
+    /**
+     * Sets the corresponding walking animation based on angle to player
+     */
+    private void SetWalkingAnimation() {
+        Vector3 targetDir = target.position - transform.position;
+        float angle = Vector3.SignedAngle(targetDir, transform.forward, Vector3.up);
+
+        if (angle <= 43.75 && angle >= -43.75) {
+            animator.SetBool("Forward", true);
+        }
+        else if (angle < 131.25 && angle > 43.75) {
+            animator.SetBool("Right", true);
+        }
+        else if ((angle <= 180 && angle >= 131.25) || (angle <= -131.25 && angle >= -180)) {
+            animator.SetBool("Backward", true);
+        }
+        else if (angle < -43.75 && angle > -131.25) {
+            animator.SetBool("Left", true);
+        }
+    }
+
+    /**
+     * Reset all booealn parameters for animation states except for Idle
+     */
+    private void ResetAnimations() {
+        animator.SetBool("Forward", false);
+        animator.SetBool("Backward", false);
+        animator.SetBool("Left", false);
+        animator.SetBool("Right", false);
+        animator.SetBool("Idle", false);
+    }
+
+
+
+
 
 
 
